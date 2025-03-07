6bc71eef3460767457c76cc24efeedbdebb84d01
Arthur <arthur.vanroost@icloud.com>
2025-03-07 13:52:47 +0100

    exo3 fin

--- exo3.sh
+++ exo3.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+echo "donne un nom de fichier"
+read fichier
+echo "confirme par oui/non"
+read reponse
+if [ "$reponse" != "${reponse#[oui]}" ] ;then
+    touch $fichier
+else
+    echo "Au revoir"
+    exit
+fi
+
+echo "Fichier $fichier crée avec succes"
